9be02b9a39df9a08d48b0e6ade2f7501e779a8d2
Joonchul <jjcminy@naver.com>
2024-01-19 13:51:33 +0900

    cors config update : expose headers to login

--- src/main/java/com/packt/cardatabase/SecurityConfig.java
+++ src/main/java/com/packt/cardatabase/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.packt.cardatabase;
 
-
 import com.packt.cardatabase.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -24,51 +23,50 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
 
-    @Autowired
-    private AuthenticationFilter authenticationFilter;
+	@Autowired
+	private AuthenticationFilter authenticationFilter;
 
-    @Autowired
-    private AuthEntryPoint exceptionHandler;
+	@Autowired
+	private AuthEntryPoint exceptionHandler;
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+	}
 
-    @Bean
-    public AuthenticationManager getAuthenticationManager() throws Exception{
-        return authenticationManager();
-    }
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception {
+		return authenticationManager();
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().cors().and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests()
-                // /login 엔드포인트에 대한 POST 요청은 보호되지 않음
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
-                // 다른 모든 요청은 보호됨
-                .anyRequest().authenticated().and()
-                .exceptionHandling()
-                .authenticationEntryPoint(exceptionHandler).and()
-                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().cors().and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.POST, "/login").permitAll()
+		.anyRequest().authenticated().and()
+		.exceptionHandling()
+		.authenticationEntryPoint(exceptionHandler).and()
+		.addFilterBefore(authenticationFilter, 
+				UsernamePasswordAuthenticationFilter.class);
+	}
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource(){
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("*"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowCredentials(false);
-        config.applyPermitDefaultValues();
-
-        source.registerCorsConfiguration("/**",config);
-        return source;
-    }
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowCredentials(false);
+		config.applyPermitDefaultValues();
+		config.addExposedHeader("Authorization");
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
 }
